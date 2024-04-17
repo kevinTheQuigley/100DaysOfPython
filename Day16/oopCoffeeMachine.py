4cfca8afd28e3463830e0c7265a505a407c0a5eb
kevinTheQuigley <kevinthomassssquigley@gmail.com>
2024-04-17 12:20:33 +0100

    OOP coffe machine update

--- Day16/oopCoffeeMachine.py
+++ Day16/oopCoffeeMachine.py
@@ -8,11 +8,23 @@ coffeeMachine = CoffeeMaker()
 #espresso = MenuItem("espresso",100,100,100,1)
 #americano= MenuItem("americano",100,100,100,1)
 theMenu = Menu()
+coin_machine = MoneyMachine()
+is_machine_on = True
 
 
+while is_machine_on==True:
+    user_input = input("Hi and welcome to the OOP coffee machine!\n What kind of coffee would you like?:- ").lower()
+    if user_input == "off":
+        is_machine_on = False
+    elif user_input == "report":
+        coffeeMachine.report()
+        coin_machine.report()
+    else:
+        drink = theMenu.find_drink(user_input)
+        if coffeeMachine.is_resource_sufficient(drink):
+            coin_machine.make_payment(drink.cost)
+            print()
+            coffeeMachine.make_coffee(drink)
 
-coffeeMachine.report()
-
-#coffeeMachine.is_resource_sufficient("latte")
-
-print(theMenu.get_items())
+''' 
+'''
