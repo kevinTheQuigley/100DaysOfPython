60dabfbf0b55acac246575ad82c798e8844471af
Kevin Quigley <kevinthomassssquigley@gmail.com>
2024-05-05 20:58:38 +0100

    Day 31 continued

--- Day31/main.py
+++ Day31/main.py
@@ -20,21 +20,28 @@ data = pandas.read_csv("data/french_words.csv")
 print(data)
 # Start with card front
 canvas = Canvas(height=526, width=800)
-card_front = PhotoImage(file="images\\card_front.png")
-canvas.create_image(426, 300, image=card_front)
 canvas.grid(row=0, column=0,columnspan = 2)
 
-top_text = canvas.create_text(400,150, text = "French",fill = "black",font =TOP_FONT_NAME )
+card_front = PhotoImage(file="images\\card_front.png")
+card_back = PhotoImage(file="images\\card_back.png")
+canvas.create_image(426, 300, image=card_front)
 bottom_text = canvas.create_text(400,263, text = "French",fill = "black",font =BOTTOM_FONT_NAME )
 #canvas.itemconfig(timer_text,text  = time.strftime("%M:%S",time.gmtime(count)))
 def refresh_cards():
     global canvas
     global bottom_text
+    global card_front
+    global card_back
+    top_text = canvas.create_text(400,150, text = "French",fill = "black",font =TOP_FONT_NAME )
     rd_data =  data.loc[random.randint(0,100)]
     rd_start = rd_data['French']
-    rd_end = rd_data['English']
     canvas.itemconfig(bottom_text,text =  rd_start)
 
+    time.sleep(3)
+
+    rd_end = rd_data['English']
+
+    canvas.itemconfig(bottom_text,text =  rd_end)
 
 def right_button_click():
     refresh_cards()
