5630c94cb8fae4ff3534d606696ca33048a82cf4
Kevin Quigley <kevinthomassssquigley@gmail.com>
2024-05-07 09:45:10 +0100

    Wrapping up word removal

--- Day31/main.py
+++ Day31/main.py
@@ -16,34 +16,75 @@ window.title("Password Manager")
 window.config(padx=50, pady=50)
 
 # Setting up the data
-data = pandas.read_csv("data/french_words.csv")
-print(data)
+data = pandas.read_csv("data/words_to_learn.csv")
 # Start with card front
 canvas = Canvas(height=526, width=800)
 canvas.grid(row=0, column=0,columnspan = 2)
 
 card_front = PhotoImage(file="images\\card_front.png")
 card_back = PhotoImage(file="images\\card_back.png")
-canvas.create_image(426, 300, image=card_front)
-bottom_text = canvas.create_text(400,263, text = "French",fill = "black",font =BOTTOM_FONT_NAME )
+
+#asdf
+canvas_image = canvas.create_image(426, 300, image=card_front)
+#rd_data =  data.loc[random.randint(0,100)]
+#rd_start = rd_data['French']
+
+
+#bottom_text = canvas.create_text(400,263, text = rd_start,fill = "black",font =BOTTOM_FONT_NAME )
+top_text_start = canvas.create_text(400,150, text = "French",fill = "black",font =TOP_FONT_NAME )
+#top_text_end = canvas.create_text(400,150, text = "English",fill = "black",font =TOP_FONT_NAME )
+
+bottom_text = canvas.create_text(400,263, text = "start",fill = "black",font =BOTTOM_FONT_NAME )
+
 #canvas.itemconfig(timer_text,text  = time.strftime("%M:%S",time.gmtime(count)))
+data_dict = {}
+rd_data = {}
 def refresh_cards():
     global canvas
-    global bottom_text
+    global top_text_start
     global card_front
     global card_back
-    top_text = canvas.create_text(400,150, text = "French",fill = "black",font =TOP_FONT_NAME )
+    global canvas_image	
+    global window
+    global data_dict
+    if 'window_after' in globals():
+        window.after_cancel(window_after)
+    global flip_back
     rd_data =  data.loc[random.randint(0,100)]
+    data_dict = rd_data
     rd_start = rd_data['French']
-    canvas.itemconfig(bottom_text,text =  rd_start)
+    canvas.itemconfig(bottom_text,text =  rd_start, fill = "black")
+    canvas.itemconfig(top_text_start,text =  "French", fill = "black")
+    canvas.itemconfig(canvas_image,image =card_front   )
 
-    time.sleep(3)
+    window_after = window.after(3000,flip_back,rd_data)
+    #global window_after
 
-    rd_end = rd_data['English']
+#refresh_cards()
+def flip_back(rd_data):
+    global canvas
+    global bottom_text
+    canvas.itemconfig(canvas_image,image =card_back)
+    try: 
+        rd_end = rd_data['English']
+    except(KeyError):
+        pass
+        
 
-    canvas.itemconfig(bottom_text,text =  rd_end)
+    canvas.itemconfig(bottom_text,text =  rd_end,fill = "white")
+    canvas.itemconfig(top_text_start,text =  "English", fill = "white")
+
+    #window.after_cancel()
 
 def right_button_click():
+    global data
+    global rd_data
+    try:
+        data =data.drop(index = rd_data["French"] )
+        data.to_csv("data/words_to_lean.csv",index = False)
+        print(len(data))
+    except(KeyError):
+        pass
     refresh_cards()
     
 
