e75a603de41b38d2bc6a46b6eba2d47d92e5b985
Kevin Quigley <kevinthomassssquigley@gmail.com>
2024-05-19 13:21:30 +0100

    Finished skyscanner flight API

--- Day39/data_manager.py
+++ Day39/data_manager.py
@@ -1,3 +1,14 @@
+import requests
 class DataManager:
+    def __init__(self) -> None:
+        url = "https://api.sheety.co/7578b074a021d96b936b9161968196fb/kevsFlightDeals/prices"
+        self.response = requests.get(url).json()
+        self.iataCodes = []
+        self.durations = []
+        self. prices = []
+        for j in self.response['prices']:
+            self.iataCodes.append(j['iataCode'])
+            self.durations.append(j['duration'])
+            self.prices.append(j['lowestPrice'])
     #This class is responsible for talking to the Google Sheet.
     pass
--- Day39/flight_data.py
+++ Day39/flight_data.py
@@ -8,32 +8,9 @@ from arsewards import arsewards_dict
 
 
 
-class FlightData:
-    def __init__(self,destination) -> None:
+class FlightData_roundtrip:
+    def __init__(self,destination,duration) -> None:
         self.destination = destination
-    #This class is responsible for structuring the flight data.
-    #payload = {
-    #    "market": "IE",
-    #    "locale": "en-GB",
-    #    "currency": "EUR",
-    #    "adults": 1,
-    #    "children": 0,
-    #    "infants": 0,
-    #    "cabinClass": "economy",
-    #    "stops": ["direct", "1stop", "2stops"],
-    #    "flights": [
-    #        {
-    #            "fromEntityId": "DUB",
-    #            "toEntityId": "SYD",
-    #            "departDate": "2024-06-21"
-    #        },
-    #        {
-    #            "fromEntityId": "DUB",
-    #            "toEntityId": "SOF",
-    #            "departDate": "2024-06-28"
-    #        }
-    #    ]
-    #}
     querystring = {
                 "fromEntityId":"DUB",
                 "toEntityId":"SYD"
@@ -52,7 +29,8 @@ class FlightData:
 
 
     #url = "https://sky-scanner3.p.rapidapi.com/flights/search-multi-city"
-    url = "https://sky-scanner3.p.rapidapi.com/flights/search-roundtrip"
+    #url_oneway = 'https://sky-scanner3.p.rapidapi.com/flights/search-one-way',
+    url= "https://sky-scanner3.p.rapidapi.com/flights/search-roundtrip"
 
     def get_flight_data(self):
         self.response = requests.get(self.url, headers=self.headers, params=self.querystring)
@@ -61,8 +39,9 @@ class FlightData:
     
     def get_prices(self):
         self.prices = []
-        for itenerary in self.response.json()["data"]["itineraries"]:
-            self.prices.append(itenerary["price"]["raw"])
+        for itenerary in self.response.json()["data"]["flightQuotes"]["results"]:
+            #flight_data.response.json()["data"]["flightQuotes"]["results"][0]['content']['price']
+            self.prices.append(itenerary["content"]["rawPrice"])
             return(self.prices)
 
     def get_legs(self):
@@ -90,15 +69,94 @@ class FlightData:
         for itenerary in self.response.json()["data"]["itineraries"]:
             pass
 
-        
 
-flight_data = FlightData("Sydney")
-print(flight_data.get_flight_data())
+class FlightData_oneway:
+    def __init__(self,destination,duration) -> None:
+        self.destination = destination
+        self.duration =duration
 
-print(flight_data.get_prices())
-print(flight_data.get_legs())
-print(flight_data.get_carriers())
-print(flight_data.get_selfTransfers())
+        self.querystring = {
+                    "fromEntityId":"DUB",
+                    "toEntityId":self.destination,
+                    "duration":self.duration*60,
+                    "stops":0
+                    #51 hour - 3090
+                    #28 hour - 1680
+                    }
+
+    headers = {
+    	#"content-type": "application/json",
+    	"X-RapidAPI-Key": arsewards_dict['X-RapidAPI-Key'],
+    	"X-RapidAPI-Host": "sky-scanner3.p.rapidapi.com"
+    }
+
+
+    url= 'https://sky-scanner3.p.rapidapi.com/flights/search-one-way'
+
+    def get_flight_data(self):
+        self.response = requests.get(self.url, headers=self.headers, params=self.querystring)
+        return(self.response.json())
+    
+    def get_prices(self):
+        self.prices = []
+        for itenerary in self.response.json()["data"]["flightQuotes"]["results"]:
+            #flight_data.response.json()["data"]["flightQuotes"]["results"][0]['content']['price']
+            self.prices.append(itenerary["content"]["rawPrice"])
+        return(self.prices)
+    
+    
+    def get_dates(self):
+        self.dates = []
+        for itenerary in self.response.json()["data"]["flightQuotes"]["results"]:
+            #flight_data.response.json()["data"]["flightQuotes"]["results"][0]['content']['price']
+            self.dates.append(itenerary["content"]["outboundLeg"]['localDepartureDateLabel'])
+        return(self.dates)
+
+    #for itenerary in flight_data.response.json()["data"]["flightQuotes"]["results"]:
+    #    print(itenerary["content"]["price"])
+    #flight_data.response.json()["data"]["flightQuotes"]["results"][0]['content']['price']
+    #self.prices.append(itenerary["content"]["price"])
+    #return(self.prices)
+
+#    def get_legs(self):
+#        self.legs = []
+#        for itenerary in self.response.json()["data"]["flightQuotes"]["results"]:
+#            self.legs.append(itenerary["legs"][:]["destination"]["name"])
+#        return(self.legs)
+
+#    for itenerary in flight_data.response.json()["data"]["flightQuotes"]["results"]:
+#        #print(itenerary["content"]["price"])
+#        print(itenerary["legs"]["price"][:]["destination"]["name"])
+
+#    def get_carriers(self):
+#        self.carriers = []
+#        for itenerary in self.response.json()["data"]["flightQuotes"]["results"]:
+#            self.carriers.append(itenerary["legs"][:]["carriers"]["marketing"][:]["name"])
+#        return(self.carriers)
+#
+#    def get_selfTransfers(self):
+#        self.selfTransfers = []
+#        for itenerary in self.response.json()["data"]["itineraries"]:
+#            self.selfTransfers.append(itenerary["legs"]["isSelfTransfer"])
+#        return(self.selfTransfers)
+#            
+#        print(self.response.text)
+
+    def generate_text_itenerary(self):
+        self.textReturn = []
+        for itenerary in self.response.json()["data"]["itineraries"]:
+            pass
+
+
+
+#flight_data = FlightData_oneway("SYD",28)
+#print(flight_data.get_flight_data())
+#
+#print(flight_data.get_prices())
+#print(flight_data.get_dates())
+#print(flight_data.get_legs())
+#print(flight_data.get_carriers())
+#print(flight_data.get_selfTransfers())
 
 
 
