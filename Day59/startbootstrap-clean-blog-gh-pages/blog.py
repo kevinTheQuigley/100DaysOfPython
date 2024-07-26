1387a31aaaa12e198bd4d6f740cabba8b12c4aa5
Kevin Quigley <kevinthomassssquigley@gmail.com>
2024-07-26 11:35:09 +0100

    pushing new blog format

---
+++ Day59/startbootstrap-clean-blog-gh-pages/blog.py
@@ -0,0 +1,36 @@
+from flask import Flask,render_template
+import requests
+
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    blogData = requests.get("https://api.npoint.io/16c047af2b016b24c4fa").json()
+    return(render_template("index.html",blog = blogData))
+
+@app.route("/about/")
+def about():
+    return(render_template("about.html"))
+
+
+@app.route("/contact/")
+def contact():
+    return(render_template("contact.html"))
+
+@app.route("/blogPost/<int:id>")
+def returnBlogpost(id):
+    blogData = requests.get("https://api.npoint.io/16c047af2b016b24c4fa").json()
+    return(render_template("post.html",blog = blogData[int(id)+1]))
+
+
+
+while __name__ =="__main__":
+
+    blogData = requests.get("https://api.npoint.io/16c047af2b016b24c4fa")
+    app.run(debug=True)
+
+
+#<a href="{{url_for('about')}}">Read</a>
+#          <a href="{{url_for('get_blog', id =blogPost['id'])}}">Read</a>
